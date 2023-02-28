7e56fbc5b878dfa81566c3fd202c1b83a1c99d73
fitsaleem <sherohero33@gmail.com>
2023-02-28 17:35:45 +0500

    publish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ let center="center"
 function App({Title}){
 
     let obj={
-        Name:"react-js course for bignners" ,Views:"500K" ,time:"1 years"
+        Name:"react-js course for bignners", 
+        Views:"500K" ,
+        time:"1 years",
+        tick: false
     }
     
 return (
@@ -13,7 +16,7 @@ return (
     <div className="container">
 
 <Videos  {...obj} ></Videos>
-<Videos  Name="javaScript course for biggners" Views="500K" time="2 years"></Videos>
+<Videos  Name="javaScript course for biggners" Views="500K" time="2 years" tick={false}></Videos>
 <Videos  Name="nodeJS course in one video" Views="200K" time="1 months"></Videos>
 <Videos  Name="react-js Advance course" Views="10K" time="5 days"></Videos>
 
--- src/mycomponents/Videos.js
+++ src/mycomponents/Videos.js
@@ -3,18 +3,39 @@ import "./Videos.css";
 import React from "react";
 
 import "./Videos.css";
-const Videos = ({ Name, Views, time }) => {
+const Videos = ({ Name, Views, time, tick = true }) => {
   let channelName = "fitcodding";
   return (
     <>
       <div className="image">
         <div>
-        <iframe width="250" height="150" src="https://www.youtube.com/embed/NJ-he3DBpCE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe
+            width="250"
+            height="150"
+            src="https://www.youtube.com/embed/NJ-he3DBpCE"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </div>
+        {/* first method */}
+        {/* {tick ?(
+          <div>
+            {channelName} <span className="tick">&#10004;</span>
+          </div>
+        ) : (
+          <div>{channelName}</div>
+        )} */}
+
+        {/* second method */}
+
+        <div>
+            {channelName} <span className="tick"> {tick ? "✔" : null } </span>
+          </div>
 
         <div> {Name} </div>
         <div className="videoDetails">
-          <div>{channelName}</div>
           <div>
             Views: {Views} . {time}
           </div>
